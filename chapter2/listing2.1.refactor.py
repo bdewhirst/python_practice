6cc8c38548cf4b70454646ad416d941da3827a49
bdewhirst <b.dewhirst@gmail.com>
2020-02-09 13:51:36 -0500

    Complete refactoring exercise

--- chapter2/listing2.1.refactor.py
+++ chapter2/listing2.1.refactor.py
@@ -1,4 +1,4 @@
-# this is my work on the exercise.
+# this is my solution to the exercise
 # Listing 2.1 (cleaning up) Shoddy procedural code
 import random
 
@@ -10,7 +10,7 @@ def player_choose() -> str:
     Display choices and ask human player to pick
     """
     print("(1) Rock\n(2) Paper\n(3) Scissors")
-    choice: int = OPTIONS[int(input("Enter the number of your choice: ")) - 1]
+    choice: str = OPTIONS[int(input("Enter the number of your choice: ")) - 1]
     return choice
 
 
@@ -26,38 +26,56 @@ def choose(player: str,) -> str:
     return choice
 
 
+def announce_pick(player: str, choice: str) -> None:
+    """
+    Announce player or computer's choice
+    """
+    if player == "human":
+        print(f"You chose {choice}")
+    else:
+        print(f"The computer chose {choice}")
+
+
+def score_game(human_choice: str, computer_choice: str, human_beats: str, human_loses: str) -> None:
+    """
+    Print outcome of non-draw games
+    """
+    if computer_choice == human_beats:
+        print("Yes,", human_choice, "beat", computer_choice)
+    elif computer_choice == human_loses:
+        print("Sorry,", computer_choice, "beat", human_choice)
+
+
+def determine_outcome(human_choice: str, computer_choice: str) -> None:
+    """
+    Output results of the game
+    """
+    if human_choice == computer_choice:
+        print("Draw!")
+
+    if human_choice == "rock":
+        score_game(
+            human_choice=human_choice, computer_choice=computer_choice, human_beats="scissors", human_loses="paper"
+        )
+    elif human_choice == "paper":
+        score_game(
+            human_choice=human_choice, computer_choice=computer_choice, human_beats="rock", human_loses="scissors"
+        )
+    elif human_choice == "scissors":
+        score_game(human_choice=human_choice, computer_choice=computer_choice, human_beats="paper", human_loses="rock")
+
+
 def play_rock_paper_scissors() -> None:
     """
     Play one round of rock paper scissors
     """
     human_choice = choose(player="human")
-    # refactor this printing bit
-    print(f"You chose {human_choice}")
+    # print(f"You chose {human_choice}")
     computer_choice = choose(player="ai")
-    print(f"The computer chose {computer_choice}")
-
-    # next section to refactor
-    if human_choice == "rock":
-        if computer_choice == "paper":
-            print("Sorry, paper beat rock")
-        elif computer_choice == "scissors":
-            print("Yes, rock beat scissors!")
-        else:
-            print("Draw!")
-    elif human_choice == "paper":
-        if computer_choice == "scissors":
-            print("Sorry, scissors beat paper")
-        elif computer_choice == "rock":
-            print("Yes, paper beat rock!")
-        else:
-            print("Draw!")
-    elif human_choice == "scissors":
-        if computer_choice == "rock":
-            print("Sorry, rock beat scissors")
-        elif computer_choice == "paper":
-            print("Yes, scissors beat paper!")
-        else:
-            print("Draw!")
+    # print(f"The computer chose {computer_choice}")
+    for player, choice in {"human": human_choice, "computer": computer_choice}.items():
+        announce_pick(player, choice)
+    determine_outcome(human_choice=human_choice, computer_choice=computer_choice)
 
 
 play_rock_paper_scissors()
