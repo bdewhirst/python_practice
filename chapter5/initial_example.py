f33a94f21f9d095638114691af0691cefeb8d45a
bdewhirst <b.dewhirst@gmail.com>
2020-02-22 14:19:40 -0500

    Record interactive lesson

---
+++ chapter5/initial_example.py
@@ -0,0 +1,19 @@
+# This script contains the code from the interactive REPL examples
+
+def calculate_mean(numbers: list) -> float:
+    """
+    Example function for purposes of explaining testing concepts
+    """
+    if not numbers:
+        return 0
+    return sum(numbers) / len(numbers)
+
+
+# these pass
+assert calculate_mean([]) == 0.0
+assert calculate_mean([1,2,3,4]) == 2.5
+assert calculate_mean([1,0,-1]) == 0.0
+assert calculate_mean([0])  == 0.0
+
+# this fails
+assert calculate_mean([1000, 7000, 7000000]) == 1.0
