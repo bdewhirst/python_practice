a29c6ab3ca7ebebfe097003872d25af3f5382797
bdewhirst <b.dewhirst@gmail.com>
2020-02-22 14:44:49 -0500

    Clean up minimal example unittest

---
+++ chapter5/listing_5.1_exercise/test_product.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class ProductTestCase(unittest.TestCase):
+    def test_working(self) -> None:
+        """
+        Example of a passing test
+
+        Details:
+            - unittest would find zero tests if ProductTestCase had no methods
+            - unittest also expects collections of one or more tests to be within unittest.TestCase objects
+        """
+        pass
