c844fa9d9c036dff5f8fa50f77fae4c1431400c3
bdewhirst <b.dewhirst@gmail.com>
2020-03-01 11:24:13 -0500

    Add notes and down tools for now

---
+++ chapter6/bark.py
@@ -0,0 +1,13 @@
+
+# info to store:
+# id
+# title
+# url
+# notes -- he wants to save why he bookmarked something
+# date added
+
+# methods
+# list all bookmarks
+# add new bookmark
+# delete specific bookmark
+# on startup, it will list options
