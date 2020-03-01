42ad80c2ca509bd9581fa154201451620f169630
bdewhirst <b.dewhirst@gmail.com>
2020-03-01 09:54:30 -0500

    Create tests for product menthods

---
+++ chapter5/listing_5.3_exercise/test_product.py
@@ -0,0 +1,40 @@
+import unittest
+
+import product
+
+
+def make_test_sku():
+    """
+    Create a sample SKU based on the docstring example
+    """
+    p = product.Product(name="shoes", size="S", color="black")
+    return p
+
+
+class ProductTestCases(unittest.TestCase):
+    def test_name_case(self) -> None:
+        """
+        Test method transform_name_for_sku returns upper case
+        """
+        p = make_test_sku()
+        expected_value = "SHOES"
+        actual_value: str = p.transform_name_for_sku()
+        self.assertEqual(expected_value, actual_value)
+
+    def test_color_case(self) -> None:
+        """
+        Test method transform_color_for_sku return upper case
+        """
+        p = make_test_sku()
+        expected_value = "BLACK"
+        actual_value = p.transform_color_for_sku()
+        self.assertEqual(expected_value, actual_value)
+
+    def test_generate_sku(self):
+        """
+        Test method transform_name_for_sku
+        """
+        p = make_test_sku()
+        expected_value = "SHOES-S-BLACK"
+        actual_value = p.generate_sku()
+        self.assertEqual(expected_value, actual_value)
