9a0b7dd97b7b2bfbef234a5b0285797d69751421
bdewhirst <b.dewhirst@gmail.com>
2020-03-14 17:08:30 -0400

    Complete business logic functionality

--- chapter6/business_logic.py
+++ chapter6/business_logic.py
@@ -1,3 +1,5 @@
+import sys
+
 import datetime as dt
 
 import database_manager
@@ -8,7 +10,7 @@ db = database_manager.DatabaseManager(database_filename="bookmarks.db")
 
 class CreateBookmarksTableCommand:
     """
-    Creates database table with loose coupling and separation of concerns
+    Create database table with loose coupling and separation of concerns
     """
     def execute(self) -> None:
         cols = {
@@ -22,10 +24,37 @@ class CreateBookmarksTableCommand:
 
 
 class AddBookmarkCommand(self, bookmark_data):
-    def execute(self, bookmark_data: dict) -> str:
-        """
-        Update database table with loose coupling and separation of concerns
-        """
+    """
+    Update database table with loose coupling and separation of concerns
+    """
+    def execute(self, bookmark_data=bookmark_data) -> str:
         bookmark_data['date_added'] = dt.datetime.utcnow().isoformat()
         db.add(table_name='bookmarks', data=bookmark_data)
         return 'Bookmark added!'
+
+
+class ListBookmarksCommand:
+    """
+    Retrieve stored bookmarks
+    """
+    def __init__(self, order_by='date_added'):
+        self.order_by = order_by
+
+    def execute(self):
+        return db.select('bookmarks', order_by=self.order_by).fetchall()
+
+
+class DeleteBookmarkCommand:
+    """
+    Remove bookmark
+    """
+    def execute(self, id_to_remove) -> str:
+        db.delete('bookmarks', {'id': id_to_remove})
+        return 'Bookmark deleted!'
+
+class QuitCommand:
+    """
+    Quit out of the program
+    """
+    def execute(self):
+        sys.exit()
