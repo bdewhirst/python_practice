899d7b96067f8c33794f39d22ba208b9acea7739
bdewhirst <b.dewhirst@gmail.com>
2020-03-14 17:39:07 -0400

    Create CLI presentation layer

--- chapter6/commands.py
+++ chapter6/commands.py
@@ -12,6 +12,7 @@ class CreateBookmarksTableCommand:
     """
     Create database table with loose coupling and separation of concerns
     """
+
     def execute(self) -> None:
         cols = {
             "id": "integer primary key autoincrement",
@@ -20,41 +21,48 @@ class CreateBookmarksTableCommand:
             "notes": "text",
             "date_added": "text not null",
         }
-        db.create_table(table_name="bookmarks", columns=cols,)
+        db.create_table(
+            table_name="bookmarks", columns=cols,
+        )
 
 
 class AddBookmarkCommand(self, bookmark_data):
     """
     Update database table with loose coupling and separation of concerns
     """
+
     def execute(self, bookmark_data=bookmark_data) -> str:
-        bookmark_data['date_added'] = dt.datetime.utcnow().isoformat()
-        db.add(table_name='bookmarks', data=bookmark_data)
-        return 'Bookmark added!'
+        bookmark_data["date_added"] = dt.datetime.utcnow().isoformat()
+        db.add(table_name="bookmarks", data=bookmark_data)
+        return "Bookmark added!"
 
 
 class ListBookmarksCommand:
     """
     Retrieve stored bookmarks
     """
-    def __init__(self, order_by='date_added'):
+
+    def __init__(self, order_by="date_added"):
         self.order_by = order_by
 
     def execute(self):
-        return db.select('bookmarks', order_by=self.order_by).fetchall()
+        return db.select("bookmarks", order_by=self.order_by).fetchall()
 
 
 class DeleteBookmarkCommand:
     """
     Remove bookmark
     """
+
     def execute(self, id_to_remove) -> str:
-        db.delete('bookmarks', {'id': id_to_remove})
-        return 'Bookmark deleted!'
+        db.delete("bookmarks", {"id": id_to_remove})
+        return "Bookmark deleted!"
+
 
 class QuitCommand:
     """
     Quit out of the program
     """
+
     def execute(self):
         sys.exit()
---
+++ chapter6/presentation_layer.py
@@ -0,0 +1,36 @@
+import commands
+
+
+class Option:
+    def __init__(self, name, command, prep_call=None):
+        self.name = name
+        self.command = command
+        self.prep_call = prep_call
+
+        def choose(self):
+            data = self.prep_call() if self.prep_call else None
+            message = self.command.execute(data) if data else self.command.execute()
+            print(message)
+
+        def __str__(self):
+            return self.name
+
+
+def print_options(options):
+    for shortcut, option in options.items():
+        print(f"({shortcut}) {option}")
+        print()
+
+
+if __name__ == "__main__":
+    print("Welcome to bookmarking widget!")
+    commands.CreateBookmarksTableCommand().execute()
+
+    options = {
+        "A": Option("Add a bookmark", commands.AddBookmarkCommand()),
+        "B": Option("List bookmars by date", commands.ListBookmarksCommand()),
+        "T": Option("List bookmarks by title", commands.ListBookmarksCommand(order_by="title")),
+        "D": Option("Delete a bookmark", commands.DeleteBookmarkCommand()),
+        "Q": Option("Quit", commands.QuitCommand()),
+    }
+    print_options(options)
