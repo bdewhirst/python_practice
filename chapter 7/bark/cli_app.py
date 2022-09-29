0be46870bb130c7188527609102a0d7452a2417c
bdewhirst <b.dewhirst@gmail.com>
2022-09-29 11:04:25 -0400

    add bark from chapter 6

---
+++ chapter 7/bark/cli_app.py
@@ -0,0 +1,126 @@
+import os
+from collections import OrderedDict
+
+import commands
+
+
+def print_bookmarks(bookmarks):
+    for bookmark in bookmarks:
+        print("\t".join(str(field) if field else "" for field in bookmark))
+
+
+class Option:
+    def __init__(self, name, command, prep_call=None):
+        self.name = name
+        self.command = command
+        self.prep_call = prep_call
+
+    def _handle_message(self, message):
+        if isinstance(message, list):
+            print_bookmarks(message)
+        else:
+            print(message)
+
+    def choose(self):
+        data = self.prep_call() if self.prep_call else None
+        message = self.command.execute(data) if data else self.command.execute()
+        self._handle_message(message)
+
+    def __str__(self):
+        return self.name
+
+
+def print_options(options):
+    """
+    Displays the supported options and the shortcut
+    """
+    for shortcut, option in options.items():
+        print(f"({shortcut}) {option}")
+    print()
+
+
+def option_choice_is_valid(choice, options):
+    """
+    Checks if selected choice is a valid option
+    """
+    return choice in options or choice.upper() in options
+
+
+def get_option_choice(options):
+    """
+    Prompt user to make a choice until they quit out
+    """
+    choice = input("Choose an option: ")
+    while not option_choice_is_valid(choice=choice, options=options):
+        print("Invalid choice")
+        choice = input("Choose an option: ")
+    return options[choice.upper()]
+
+
+def get_user_input(label, required=True):
+    value = input(f"{label}: ") or None
+    while required and not value:
+        value = input(f"{label}: ") or None
+    return value
+
+
+def get_new_bookmark_data():
+    return {
+        "title": get_user_input(label="Title"),
+        "url": get_user_input(label="URL"),
+        "notes": get_user_input(label="Notes", required=False),
+    }
+
+
+def get_bookmark_id_for_deletion():
+    return get_user_input("Enter a bookmark ID to delete")
+
+
+def clear_screen():
+    """
+    Clear the screen before re-printing the menu
+    """
+    clear = "cls" if os.name == "nt" else "clear"
+    os.system(clear)
+
+
+def loop():
+    clear_screen()
+    print("Welcome to Bark, the bookmarking widget!")
+
+    options = OrderedDict(
+        {
+            "A": Option(
+                name="Add a bookmark",
+                command=commands.AddBookmarkCommand(),
+                prep_call=get_new_bookmark_data,
+            ),
+            "B": Option(
+                name="List bookmarks by date",
+                command=commands.ListBookmarksCommand(),
+            ),
+            "T": Option(
+                name="List bookmarks by title",
+                command=commands.ListBookmarksCommand(order_by="title"),
+            ),
+            "D": Option(
+                name="Delete a bookmark",
+                command=commands.DeleteBookmarkCommand(),
+                prep_call=get_bookmark_id_for_deletion,
+            ),
+            "Q": Option(name="Quit", command=commands.QuitCommand()),
+        }
+    )
+    print_options(options)
+    chosen_option = get_option_choice(options)
+    clear_screen()
+    chosen_option.choose()
+
+    _ = input("Press Enter to return to menu")
+
+
+if __name__ == "__main__":
+    # This is the presentation layer
+    commands.CreateBookmarksTableCommand().execute()
+    while True:
+        loop()
---
+++ chapter 7/bark/commands.py
@@ -0,0 +1,71 @@
+import sys
+import datetime as dt
+
+import database_manager
+
+
+db = database_manager.DatabaseManager(database_filename="bookmarks.db")
+
+
+class CreateBookmarksTableCommand:
+    """
+    Create database table with loose coupling and separation of concerns
+    """
+
+    def execute(self):
+        cols = {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        }
+        db.create_table(
+            table_name="bookmarks",
+            columns=cols,
+        )
+
+
+class AddBookmarkCommand:
+    """
+    Update database table with loose coupling and separation of concerns
+    """
+
+    def execute(self, bookmark_data):
+        bookmark_data["date_added"] = dt.datetime.utcnow().isoformat()
+        db.add(table_name="bookmarks", data=bookmark_data)
+        return "Bookmark added!"
+
+
+class ListBookmarksCommand:
+    """
+    Retrieve stored bookmarks
+    """
+
+    def __init__(self, order_by="date_added"):
+        self.order_by = order_by
+
+    def execute(self):
+        return db.select("bookmarks", order_by=self.order_by).fetchall()
+
+
+class DeleteBookmarkCommand:
+    """
+    Remove bookmark
+    """
+
+    def execute(self, id_to_remove):
+        db.delete("bookmarks", {"id": id_to_remove})
+        return "Bookmark deleted!"
+
+
+class QuitCommand:
+    """
+    Quit out of the program
+    """
+
+    def execute(self):
+        sys.exit()
+
+
+# Business logic layer
