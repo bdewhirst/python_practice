c6db8ead7fcabe463f0d3ac4c80408a17ce0f1b3
bdewhirst <b.dewhirst@gmail.com>
2022-09-29 10:25:48 -0400

    refactor for inversion of control, loose coupling

--- chapter 7/bicycle.py
+++ chapter 7/bicycle.py
@@ -9,15 +9,15 @@ class Frame:
         return "an aluminum frame"
 
 class Bicycle:
-    def __init__(self):
-        self.front_tire = Tire()
-        self.back_tire = Tire()
-        self.frame = Frame()
+    def __init__(self, front_tire, back_tire, frame):
+        self.front_tire = front_tire
+        self.back_tire = back_tire
+        self.frame = frame
 
     def print_specs(self):
         print(f"Frame: {self.frame}")
         print(f"Front Tire: {self.front_tire} back tire: {self.back_tire}")
 
 if __name__ == "__main__":
-    bike =  Bicycle()
+    bike =  Bicycle(Tire(), Tire(), Frame())
     bike.print_specs()
