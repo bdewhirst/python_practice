09b084f9162621bd71838b1872f4b73d6d2db9d0
bdewhirst <b.dewhirst@gmail.com>
2022-10-05 16:30:48 -0400

    simple abstract base class exercise

---
+++ chapter8/learn_abcs.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Predator(ABC):
+    @abstractmethod
+    def eat(self, prey):
+        pass
+
+
+class Bear(Predator):
+    def eat(self, prey):
+        print(f"Mauling {prey}!")
+
+class Owl(Predator):
+    def eat(self, prey):
+        print(f"Swooping in on {prey}!")
+
+class Chameleon(Predator):
+    def eat(self, prey):
+        print(f"Shooting tongue at {prey}!")
+
+if __name__ == "__main__":
+    bear = Bear()
+    bear.eat("deer")
+    owl = Owl()
+    owl.eat("mouse")
+    chameleon = Chameleon()
+    chameleon.eat("fly")
