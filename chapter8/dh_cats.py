82133f052f40fe43c53eca1d73506ed3f852bccc
bdewhirst <b.dewhirst@gmail.com>
2022-10-05 16:15:38 -0400

    resolution order and multiinheritance exercise

---
+++ chapter8/dh_cats.py
@@ -0,0 +1,25 @@
+class BigCat:
+    def eats(self):
+        return ['rodents']
+
+class Lion(BigCat):
+    def eats(self):
+        return ['wildebeest']
+
+class Tiger(BigCat):
+    def eats(self):
+        return ['water buffalo']
+
+class Liger(Lion, Tiger):
+    def eats(self):
+        return super().eats() + ['rabbit', 'cow', 'pig', 'chicken',]
+
+if __name__ == '__main__':
+    lion = Lion()
+    print("The lion eats", lion.eats())
+    tiger = Tiger()
+    print("The tiger eats", tiger.eats())
+    liger = Liger()
+    print("The liger eats", liger.eats())
+    # before running-- should be all of the enumerated critters from tiger, lion, and liger-- and rodents? (no.)
+    # ... I gather the author is about to explain inheritance priority (term of art is resolution order, but basically)
