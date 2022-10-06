2d8a356048504f5ff0b8d1d23f6ebd5e65488a8a
bdewhirst <b.dewhirst@gmail.com>
2022-10-06 15:03:10 -0400

    refactor to separate concerns and annotate

--- chapter9/random_food.py
+++ chapter9/random_food.py
@@ -2,21 +2,29 @@ import random
 import json
 
 
-FOODS = ["pizza", "tacos", "tvp",]
+FOODS = [
+    "pizza",
+    "tacos",
+    "tvp",
+]
+
+
+def to_json(food):
+    return json.dumps({"food": food})
+
+
+def to_xml(food):
+    return f"<response><food>{food}</food></response>"
 
 
 def random_food(request):
     food = random.choice(FOODS)
-
     formats = {
-        "application/json": json.dumps({"food": food}),
-        "application/xml": f"<response><food>{food}</food></response>",
+        "application/json": to_json,
+        "application/xml": to_xml,
     }
+    format_function = formats.get(request.headers.get("Accept"), lambda val: val)
+    # i.e., lambda to return 'food' if app not found
+    # signature of .get dictionary method is Dict.get(key, default=None) -- so if you provide 2nd value, uses as default
 
-    return formats.get(request.headers.get("Accept"), food)
-    # if request.headers.get("Accept") == "application/json":
-    #     return json.dumps({"food": food})
-    # elif request.headers.get("Accept") =='application/xml':
-    #     return
-    # else:
-    #     return food
+    return format_function(food)
