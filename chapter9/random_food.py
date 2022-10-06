2e44ca34422e54188405e123ffdf7441a002a416
bdewhirst <b.dewhirst@gmail.com>
2022-10-06 15:26:04 -0400

    refactor to extract inline function

--- chapter9/random_food.py
+++ chapter9/random_food.py
@@ -17,14 +17,20 @@ def to_xml(food):
     return f"<response><food>{food}</food></response>"
 
 
-def random_food(request):
-    food = random.choice(FOODS)
+def get_format_function(accept=None):
+    """
+    A comment on the signature of the .get dictionary method:
+    signature of .get dictionary method is Dict.get(key, default=None)
+        so if you provide 2nd value (like a lambda function), it uses that as default
+    """
     formats = {
         "application/json": to_json,
         "application/xml": to_xml,
     }
-    format_function = formats.get(request.headers.get("Accept"), lambda val: val)
-    # i.e., lambda to return 'food' if app not found
-    # signature of .get dictionary method is Dict.get(key, default=None) -- so if you provide 2nd value, uses as default
+    return formats.get(accept, lambda val: val)
 
+
+def random_food(request):
+    food = random.choice(FOODS)
+    format_function = get_format_function(accept=request.headers.get("Accept"))
     return format_function(food)
