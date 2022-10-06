c3de496306a74541693412a3b3b563d87fc0bd98
bdewhirst <b.dewhirst@gmail.com>
2022-10-06 14:09:37 -0400

    refactor to reduce complexity, improve readability

--- chapter9/random_food.py
+++ chapter9/random_food.py
@@ -8,10 +8,15 @@ FOODS = ["pizza", "tacos", "tvp",]
 def random_food(request):
     food = random.choice(FOODS)
 
-    if request.headers.get("Accept") == "application/json":
-        return json.dumps({"food": food})
-    elif request.headers.get("Accept") =='application/xml':
-        return f"<response><food>{food}</food></response>"
-    else:
-        return food
-    return food
+    formats = {
+        "application/json": json.dumps({"food": food}),
+        "application/xml": f"<response><food>{food}</food></response>",
+    }
+
+    return formats.get(request.headers.get("Accept"), food)
+    # if request.headers.get("Accept") == "application/json":
+    #     return json.dumps({"food": food})
+    # elif request.headers.get("Accept") =='application/xml':
+    #     return
+    # else:
+    #     return food
