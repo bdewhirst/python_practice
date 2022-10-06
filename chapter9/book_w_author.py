ba817a5e0c162a8853d440f97916d176b85cbfd3
bdewhirst <b.dewhirst@gmail.com>
2022-10-06 16:43:49 -0400

    refactor author into its own class while preserving functionality

--- chapter9/book_w_author.py
+++ chapter9/book_w_author.py
@@ -1,16 +1,31 @@
+class Author:
+    def __init__(self, data):
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+
+    @property
+    def author_for_display(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def author_for_citation(self):
+        return f"{self.last_name} {self.first_name[0]}"
+
+
 class Book:
     def __init__(self, data):
         self.title = data["title"]
         self.subtitle = data["subtitle"]
         self.author_data = data["author"]
+        self.author = Author(self.author_data)
 
     @property
     def author_for_display(self):
-        return f"{self.author_data['first_name']} {self.author_data['last_name']}"
+        return self.author.author_for_display
 
     @property
     def author_for_citation(self):
-        return f"{self.author_data['last_name']} {self.author_data['first_name'][0]}."
+        return self.author.author_for_citation
 
     @property
     def display_title(self):
