3456484b307d562ef3eb6f8a9b165e69c75a9cf5
bdewhirst <b.dewhirst@gmail.com>
2022-10-06 21:48:11 -0400

    refactor to retain separation of concerns while setting single entry point

--- chapter10/query_cleaning.py
+++ chapter10/query_cleaning.py
@@ -1,22 +1,28 @@
 import re
 
 
-def remove_spaces(query):
+def _remove_spaces(query):
     query = query.strip()
     query = re.sub(r"\s+", ' ', query)
     return query
 
-def normalize(query):
+def _normalize(query):
     query = query.casefold()  # more aggressive .lower() --> "german B" --> ss
     return query
 
-def remove_quotes(query):
+def _remove_quotes(query):
     query = re.sub(r'"', '', query)
+    return query
+
+
+def clean_query(query):
+    query = _remove_quotes(query)
+    query = _normalize(query)
+    query = _remove_quotes(query)
     return query
 
 
 if __name__ == '__main__':
     search_query = input('Enter your search query: ')
-    search_query = remove_spaces(search_query)
-    search_query = normalize(search_query)
+    search_query = clean_query(search_query)
     print(f"Running a search for '{search_query}'")
